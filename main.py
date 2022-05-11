40770cb02973a09ac29812ac4737a2555ec3420d
MinDong <mdskylover@gmail.com>
2022-05-11 17:38:30 +0900

    Remove active_page session state which was substitued with page

--- main.py
+++ main.py
@@ -5,13 +5,8 @@ import streamlit as st
 import src.center_labeller_page as center_labeller_page
 import src.labelled_page as labelled_page
 import src.quality_labeller_page as quality_labeller_page
-from src.multipage import MultiPage
 
 # TODO: downloader inject to each page
-
-
-def change_page():
-    st.session_state.active_page = st.session_state.page
 
 
 def main():
@@ -22,19 +17,14 @@ def main():
             "labelled_page": labelled_page.app,
         }
     )
-
-    if "active_page" not in st.session_state:
-        st.session_state.active_page = list(pages.keys())[0]
-
     with st.sidebar:
         st.radio(
             "App Navigation",
             list(pages.keys()),
             key="page",
-            on_change=change_page,
         )
 
-    pages[st.session_state.active_page]()
+    pages[st.session_state.page]()
 
 
 if __name__ == "__main__":
